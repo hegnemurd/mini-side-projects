49aee3e0248839a20abf1f4f5a4ea4656f7d59a6
hegnemurd <hegne.murd@gmail.com>
2021-05-19 21:30:21 +0100

    Checkout Experience Project Commit

--- checkout-experience-project/app.js
+++ checkout-experience-project/app.js
@@ -1,3 +1,18 @@
+// function loader() {
+//   document.getElementById("loader").style.display = "block";
+// }
+
+document.getElementById("pay-button").addEventListener("click", loader);
+document.getElementById("pay-button").addEventListener("click", done);
+
 function loader() {
   document.getElementById("loader").style.display = "block";
+}
+
+function done() {
+  setTimeout(function myFunction() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementsByClassName("container")[0].style.display = "none";
+    document.getElementById("done").style.display = "block";
+  }, 3000);
 }
