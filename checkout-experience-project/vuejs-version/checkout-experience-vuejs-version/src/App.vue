20c34af2117d925cae9f9e843b01ed3073983862
hegnemurd <hegne.murd@gmail.com>
2021-05-25 21:34:11 +0100

    VueJs Version - Checkout Experience Project

--- checkout-experience-project/vuejs-version/checkout-experience-vuejs-version/src/App.vue
+++ checkout-experience-project/vuejs-version/checkout-experience-vuejs-version/src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <CheckoutExperience></CheckoutExperience> -->
+    <div class="parent">
+      <div class="container">
+        <h4>£129.99</h4>
+        <quick-pay></quick-pay>
+        <div class="divider">
+          <span> OR</span>
+        </div>
+        <div class="integrations">
+          <div class="paypal">
+            <button class="paypal-button">PayPal</button>
+          </div>
+          <div class="google">
+            <button class="google-button">
+              <span class="blue">G</span><span class="red">o</span
+              ><span class="yellow">o</span><span class="blue">g</span
+              ><span class="green">l</span><span class="red">e</span> Pay
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div id="loader">
+        <span class="spinner-border text-light" role="status"></span>
+      </div>
+
+      <div id="done">Payment Successful!</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuickPay from "./components/QuickPay.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    QuickPay,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ checkout-experience-project/vuejs-version/checkout-experience-vuejs-version/src/components/QuickPay.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="quick-pay-container">
+          <div class="saved-card">
+            <button class="saved-button">
+              <i class="bi bi-credit-card"></i> Saved Card **** 4321
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault1"
+              />
+            </button>
+          </div>
+          <div class="one-click-pay">
+            <button id="pay-button">
+              One-Click Pay <i class="bi bi-lock"></i>
+            </button>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
