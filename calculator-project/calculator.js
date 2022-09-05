690dec2cbcfa77a5cb6555b39d93d128f9299ff0
hegnemurd <hegne.murd@gmail.com>
2022-09-05 21:41:53 +0100

    Changing the structure and names

--- calculator-project/calculator.js
+++ calculator-project/calculator.js
@@ -1,101 +1,72 @@
+let result = 0;
+
 numberEventList();
 charactersEventList();
 
 function numberEventList() {
   const numberButtons = document.getElementsByClassName("num-btn");
   for (let index = 0; index < numberButtons.length; index++) {
-    const buttonChar = numberButtons[index];
-    buttonChar.addEventListener("click", charClicked);
+    const buttonNum = numberButtons[index];
+    buttonNum.addEventListener("click", numClicked);
   }
 }
 
 function charactersEventList() {
-  const equationButtons = document.getElementsByClassName("equation-btn");
-  for (let index = 0; index < equationButtons.length; index++) {
-    const buttonChar = equationButtons[index];
+  const operationButtons = document.getElementsByClassName("operation-btn");
+  for (let index = 0; index < operationButtons.length; index++) {
+    const buttonChar = operationButtons[index];
     buttonChar.addEventListener("click", charClicked);
   }
 }
 
 function charClicked(event) {
-  let numValue = event.target.getAttribute("data-character");
-  // let inputValue = document.getElementById("calculation").value;
+  let charValue = event.target.getAttribute("data-operation");
+
+  document.getElementById("calculation").value += charValue;
+  console.log(charValue);
+
+  return charValue;
+}
+
+function numClicked(event) {
+  equationValue(event);
+
+  let numValue = event.target.getAttribute("data-number");
+
   document.getElementById("calculation").value += numValue;
-  const input = document.getElementById("calculation");
-  // inputValue += numValue;
   console.log(numValue);
-  // get the value of the data-number attribute
-  let inputLength = numValue.length;
-  for (let i = 0; i <= inputLength; i++) {
-    if (
-      numValue === "+" ||
-      numValue === "-" ||
-      numValue === "/" ||
-      numValue === "*"
-    ) {
-      document.getElementById("plus").disabled = true;
-      document.getElementById("minus").disabled = true;
-      document.getElementById("divide").disabled = true;
-      document.getElementById("multiply").disabled = true;
-      input.value = "";
-    }
-  }
+
   return numValue;
 }
 
 document.getElementById("clearInput").addEventListener("click", clearNum);
 document.getElementById("delete").addEventListener("click", deleteNum);
 
-document.getElementById("equal-btn").addEventListener("click", equationValue);
+document
+  .getElementById("equal-btn")
+  .addEventListener("click", equalButtonHandler);
+
+let number = 0;
 function equationValue(event) {
-  let character = event.target.getAttribute("data-character");
-  let inputValue = document.getElementById("calculation").value;
-  let inputLength = inputValue.length;
-  let result = 0;
-  let number1 = 0;
-  let number2 = 0;
-  console.log(inputValue);
+  let character = event.target.getAttribute("data-operation");
 
-  for (let i = 0; i <= inputLength; i++) {
-    if (character === "+") {
-      result = number1 + number2;
-      console.log(result);
-    } else if (character === "-") {
-      result = number1 - number2;
-      console.log(result);
-    } else if (character === "/") {
-      result = number1 / number2;
-      console.log(result);
-    } else if (character === "*") {
-      result = number1 * number2;
-      console.log(result);
-    }
+  if (character === "+") {
+    result += +number;
+  } else if (character === "-") {
+    result = number1 - number2;
+  } else if (character === "/") {
+    result = number1 / number2;
+  } else if (character === "*") {
+    result = number1 * number2;
+  } else {
+    // its a number here
+    number = character;
   }
+  // console.log(result);
+}
 
-  // console.log(inputValue);
-  // inputValue += numValue;
-  // let inputLength = numValue.length;
-  // let result = 0;
-  // let number1 = 0;
-  // let number2 = 0;
-  // for (let i = 0; i <= inputLength; i++) {
-  //   numValue = value[i];
-  //   console.log(numValue);
-  // }
-  // if (character === "+") {
-  //   result = number1 + number2;
-  //   console.log(result);
-  // } else if (character === "-") {
-  //   result = number1 - number2;
-  //   console.log(result);
-  // } else if (character === "/") {
-  //   result = number1 / number2;
-  //   console.log(result);
-  // } else if (character === "*") {
-  //   result = number1 * number2;
-  //   console.log(result);
-  // }
-  // inputValue = "";
+function equalButtonHandler() {
+  console.log("Result:", result);
 }
 
 function clearNum() {
